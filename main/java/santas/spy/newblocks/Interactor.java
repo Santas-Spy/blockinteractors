76a31c8274c8ec8ae8c69e36b6c11acffb123692
Santas-Spy <90232143+Santas-Spy@users.noreply.github.com>
2022-07-17 10:14:31 +0800

    Delete main directory

--- main/java/santas/spy/newblocks/Interactor.java
+++
@@ -1,9 +0,0 @@
-package santas.spy.newblocks;
-
-import org.bukkit.Location;
-
-public interface Interactor {
-    public abstract void interact();
-    public abstract Location getLocation();
-    public abstract String saveData();
-}
